5c0effbbb9e0e093c67500273e6abddef85a45f9
CPelzer11 <33434840+CPelzer11@users.noreply.github.com>
2018-03-10 18:37:58 -0600

    First Commit
    Uploading all the files

---
+++ README.md
@@ -0,0 +1,12 @@
+Cameron Pelzer
+3/10/2018
+
+Welcome to my foodsby web app!
+
+In order to run my app, all that has to be done is to have Node.js installed, open the command prompt, navigate to the folder with the project in it, and type in $ node app.js. Once the server is running, you can look at the webpage by going to your browser and typing in http://localhost:8888/index.html. This should direct you to the main page. There are only two pages for this website. The first is built using Javascript from restaurants.js. All the pictures and information on the page are pulled from the Javascript object notation. I copied the information from deliveries-sample.json into restaurants.js. Once there, I read through the object and used a couple loops to build the rest of the html page. There are only a few parts that are interactive:
+
+-The pills, that when selected, change what restaurants are displayed on the page.
+-The Foodsby logo in the navbar, which when clicked, bring you back to the top of index.html.
+-The buttons for each restaurant, which when clicked, redirects you to selection.html and puts the values from the index of the days and from the restaurants into the url.
+
+I then used this information to again pull the information for the restaurant selected, and display it on the new page. I also looked up all the actual photos of the restaurants to make the page look a little better. Thank you and I hope you enjoy my webpage!
---
+++ restaurants.js
@@ -0,0 +1,446 @@
+var obj = {
+  "dropoffs": [
+    {
+      "day": "Monday",
+      "deliveries": [
+        {
+          "deliveryId": 47,
+          "storeId": 1,
+          "restaurantName": "Nacho's Mexican Grill",
+          "logoUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_ndmxh07oMWDGpho7tB_Ic8N0u15M-9S6VakY9hoOsMZ3xBNM",
+          "cutoff": "10:00:00",
+          "dropoff": "11:15:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": false,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 25,
+          "storeId": 2,
+          "restaurantName": "Jersey Mike's Subs",
+          "logoUrl": "https://www.jerseymikes.com/media/static/logo.png",
+          "cutoff": "10:30:00",
+          "dropoff": "11:15:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": false,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 34,
+          "storeId": 3,
+          "restaurantName": "Mort's Delicatessen",
+          "logoUrl": "http://d3lawkbdj6aabd.cloudfront.net/singleplatform/image/upload/c_fit/5f5de245bf32de5fbdeb719b79a8cb91e6d0abf7.jpg",
+          "cutoff": "09:30:00",
+          "dropoff": "11:35:00",
+          "soldOut": false,
+          "sellingOut": true,
+          "isPastCutoff": false,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 87,
+          "storeId": 4,
+          "restaurantName": "Ham's Sandwich Shop",
+          "logoUrl": "https://pbs.twimg.com/profile_images/508990246071988224/_9FzF73r_400x400.png",
+          "cutoff": "10:30:00",
+          "dropoff": "12:00:00",
+          "soldOut": true,
+          "sellingOut": true,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        }
+      ]
+    },
+    {
+      "day": "Tuesday",
+      "deliveries": [
+        {
+          "deliveryId": 68,
+          "storeId": 5,
+          "restaurantName": "Firehouse Subs",
+          "logoUrl": "https://pbs.twimg.com/profile_images/701762584681312256/9BY6cabz_400x400.jpg",
+          "cutoff": "10:30:00",
+          "dropoff": "11:20:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 55,
+          "storeId": 6,
+          "restaurantName": "Which Wich",
+          "logoUrl": "https://upload.wikimedia.org/wikipedia/en/8/81/WhichWichLogo.gif",
+          "cutoff": "10:30:00",
+          "dropoff": "11:50:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 22,
+          "storeId": 7,
+          "restaurantName": "Gion Restaurant",
+          "logoUrl": "https://cdn.doordash.com/media/restaurant/cover/GionRestaurant_NewHope_MN.png",
+          "cutoff": "10:30:00",
+          "dropoff": "12:00:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 62,
+          "storeId": 8,
+          "restaurantName": "Living Waters Market & Cafe",
+          "logoUrl": "http://midwestmenus.com/image/logo_living_waters_cafe%2819611%29_300X200.png",
+          "cutoff": "10:15:00",
+          "dropoff": "12:20:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        }
+      ]
+    },
+    {
+      "day": "Wednesday",
+      "deliveries": [
+        {
+          "deliveryId": 87,
+          "storeId": 4,
+          "restaurantName": "Ham's Sandwich Shop",
+          "logoUrl": "https://pbs.twimg.com/profile_images/508990246071988224/_9FzF73r_400x400.png",
+          "cutoff": "10:30:00",
+          "dropoff": "12:00:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+           "deliveryId": 28,
+          "storeId": 2,
+          "restaurantName": "Jersey Mike's Subs",
+          "logoUrl": "https://www.jerseymikes.com/media/static/logo.png",
+          "cutoff": "10:30:00",
+          "dropoff": "11:15:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 37,
+          "storeId": 13,
+          "restaurantName": "Godfather's Pizza",
+          "logoUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXicq-YCOXrA_C9HuTMuzqoAXFr7q0UVluFsVFRV1Y67cFz3JEWg",
+          "cutoff": "10:15:00",
+          "dropoff": "11:55:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 62,
+          "storeId": 8,
+          "restaurantName": "Living Waters Market & Cafe",
+          "logoUrl": "http://midwestmenus.com/image/logo_living_waters_cafe%2819611%29_300X200.png",
+          "cutoff": "10:15:00",
+          "dropoff": "12:20:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        }
+      ]
+    },
+    {
+      "day": "Thursday",
+      "deliveries": [
+        {
+          "deliveryId": 28,
+          "storeId": 2,
+          "restaurantName": "Jersey Mike's Subs",
+          "logoUrl": "https://www.jerseymikes.com/media/static/logo.png",
+          "cutoff": "10:30:00",
+          "dropoff": "11:15:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 9,
+          "storeId": 9,
+          "restaurantName": "Leeann Chin",
+          "logoUrl": "https://placewise.imgix.net/images/api/retailhubjs/46f4b662c6a0ef92c6b4d2c6c1df1084",
+          "cutoff": "10:15:00",
+          "dropoff": "11:35:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 13,
+          "storeId": 10,
+          "restaurantName": "Erbert & Gerbert's Sandwich Shop",
+          "logoUrl": "https://www.erbertandgerberts.com/wp-content/themes/erbertandgerberts/images/site-logo.png",
+          "cutoff": "10:00:00",
+          "dropoff": "11:55:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        }
+      ]
+    },
+    {
+      "day": "Friday",
+      "deliveries": [
+        {
+          "deliveryId": 15,
+          "storeId": 11,
+          "restaurantName": "Granite City",
+          "logoUrl": "http://www.legacy-village.com/wp-content/uploads/2015/03/GraniteCity_Logo.png",
+          "cutoff": "10:15:00",
+          "dropoff": "11:30:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 97,
+          "storeId": 12,
+          "restaurantName": "Chili's Grill & Bar",
+          "logoUrl": "https://www.totalhappyhour.com/preview/500-450/content/files/mod.happyhour/df8a0d34b0994401909286d3582eaff8.jpg",
+          "cutoff": "10:00:00",
+          "dropoff": "11:55:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 37,
+          "storeId": 13,
+          "restaurantName": "Godfather's Pizza",
+          "logoUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXicq-YCOXrA_C9HuTMuzqoAXFr7q0UVluFsVFRV1Y67cFz3JEWg",
+          "cutoff": "10:15:00",
+          "dropoff": "11:55:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 36,
+          "storeId": 14,
+          "restaurantName": "Eddington's",
+          "logoUrl": "https://cdn.doordash.com/media/restaurant/cover/Eddingtons-Soup--Salad.png",
+          "cutoff": "10:00:00",
+          "dropoff": "12:00:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        },
+        {
+          "deliveryId": 99,
+          "storeId": 15,
+          "restaurantName": "Quiznos",
+          "logoUrl": "https://upload.wikimedia.org/wikipedia/en/thumb/4/48/Quiznos_logo.svg/1200px-Quiznos_logo.svg.png",
+          "cutoff": "10:30:00",
+          "dropoff": "12:00:00",
+          "soldOut": false,
+          "sellingOut": false,
+          "isPastCutoff": true,
+          "isOrderPlaced": false
+        }
+      ]
+    }
+  ]
+}
+
+function buildpage() {
+    var date = new Date();
+    var tday = "Monday";
+    var now = date.getTime();
+    
+    for(var day in obj.dropoffs){
+        var start = document.getElementById(obj.dropoffs[day].day);
+            
+        var h2 = document.createElement("h2");
+        h2.innerHTML = obj.dropoffs[day].day;
+            
+        var p1 = document.createElement("p");
+        p1.innerHTML = "" + obj.dropoffs[day].day + "'s Restaurant Options";
+            
+        start.appendChild(h2);
+        start.appendChild(p1);
+            
+        for(var rnum in obj.dropoffs[day].deliveries){
+            var h3 = document.createElement("h3");
+            h3.className = "col-lg-12 Rname";
+            h3.innerHTML = obj.dropoffs[day].deliveries[rnum].restaurantName;
+            
+            var picture = document.createElement("img");
+            picture.className = "col-lg-4 Rimage";
+            picture.src = obj.dropoffs[day].deliveries[rnum].logoUrl;
+            
+            var div = document.createElement("div");
+            div.className = "col-lg-6 div";
+                
+            var p2 = document.createElement("p");
+            p2.className= "col-lg-6 order";
+            p2.innerHTML = "Order Cutoff:";
+                
+            var p3 = document.createElement("p");
+            p3.className = "col-lg-2 ordertime";
+            p3.innerHTML = obj.dropoffs[day].deliveries[rnum].cutoff;
+                
+            var p4 = document.createElement("p");
+            p4.className= "col-lg-6 deliver";
+            p4.innerHTML = "Delivery Time:";
+                
+            var p5 = document.createElement("p");
+            p5.className = "col-lg-2 deliverytime";
+            p5.innerHTML = obj.dropoffs[day].deliveries[rnum].dropoff;
+                
+            var button = document.createElement("button");
+            button.type = "button";
+            button.className = "btn btn-primary col-lg-12";
+            button.innerHTML = "Select This Restaurant";
+            button.value = rnum;
+            button.title = day;
+            button.addEventListener("click", function(){ clicked(this.value, this.title); });
+            
+            
+            
+            var line = document.createElement("hr");
+            line.className = "col-lg-10";
+              
+            start.appendChild(h3);
+            start.appendChild(picture);
+            start.appendChild(div);
+            div.appendChild(p2);
+            div.appendChild(p3);
+            div.appendChild(p4);
+            div.appendChild(p5);
+            div.appendChild(button);
+            
+            if(obj.dropoffs[day].deliveries[rnum].isPastCutoff)
+                {
+                    button.disabled = true;
+                }
+            
+            if(obj.dropoffs[day].deliveries[rnum].sellingOut)
+                {
+                    var sellingout = document.createElement("p");
+                    sellingout.className = "col-lg-12 sellingout";
+                    sellingout.innerHTML = "Hurry! Almost sold out.";
+                    sellingout.style.textAlign = "center";
+                    div.appendChild(sellingout);
+                }
+            
+            if(obj.dropoffs[day].deliveries[rnum].soldOut)
+                {   
+                    var soldout = document.createElement("p");
+                    soldout.className = "col-lg-8 soldout";
+                    soldout.innerHTML = "SOLD OUT!"
+                    
+                    var soldout2 = document.createElement("p");
+                    soldout2.className = "col-lg-10 soldout2";
+                    soldout2.innerHTML = "This restaurant has hit maximum order capacity which is established to ensure quality."
+                    
+                    div.removeChild(p2);
+                    div.removeChild(p3);
+                    div.removeChild(p4);
+                    div.removeChild(p5);
+                    div.removeChild(button);
+                    div.removeChild(sellingout);
+                    
+                    div.appendChild(soldout);
+                    div.appendChild(soldout2);
+                    
+                }
+            
+            start.appendChild(line);
+            
+            }
+        
+        if(tday == "Monday"){
+            document.getElementById("M").innerHTML = "Today";
+            
+        }else if(tday == "Tuesday"){
+            document.getElementById("T").innerHTML = "Today";
+            
+        }else if(tday == "Wednesday"){
+            document.getElementById("W").innerHTML = "Today";
+            
+        }else if(tday == "Thursday"){
+            document.getElementById("TH").innerHTML = "Today";
+            
+        }else if(tday == "Friday"){
+            document.getElementById("F").innerHTML = "Today";
+        }
+            
+        }
+}
+
+function clicked(restaurant,day) {
+    window.location.assign("http://localhost:8888/selection.html?restaurant=" + restaurant + "&day=" + day + "");
+}
+
+function Select() {
+    
+    var url = window.location.href;
+    rnum = url.charAt(url.length-7);
+    day = url.charAt(url.length-1);
+    
+    var start = document.getElementById("start");
+    
+    var h3 = document.createElement("h3");
+    h3.className = "col-lg-12 Rname";
+    h3.innerHTML = obj.dropoffs[day].deliveries[rnum].restaurantName;
+            
+    var picture = document.createElement("img");
+    picture.className = "col-lg-4 Rimage";
+    picture.src = obj.dropoffs[day].deliveries[rnum].logoUrl;
+            
+    var div = document.createElement("div");
+    div.className = "col-lg-6 div";
+                
+    var p2 = document.createElement("p");
+    p2.className= "col-lg-6 order";
+    p2.innerHTML = "Order Cutoff:";
+                
+    var p3 = document.createElement("p");
+    p3.className = "col-lg-2 ordertime";
+    p3.innerHTML = obj.dropoffs[day].deliveries[rnum].cutoff;
+                
+    var p4 = document.createElement("p");
+    p4.className= "col-lg-6 deliver";
+    p4.innerHTML = "Delivery Time:";
+                
+    var p5 = document.createElement("p");
+    p5.className = "col-lg-2 deliverytime";
+    p5.innerHTML = obj.dropoffs[day].deliveries[rnum].dropoff;
+    
+    start.appendChild(h3);
+    start.appendChild(picture);
+    start.appendChild(div);
+    div.appendChild(p2);
+    div.appendChild(p3);
+    div.appendChild(p4);
+    div.appendChild(p5);
+    
+    
+}
+
